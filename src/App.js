a5f2ec9559e7c1f76976c6d883e2dfe295a4d50e
ameliawalcek <ameliawalcek@gmail.com>
2020-08-19 11:56:51 +0300

    added folders

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom
 import './App.css';
 import { observer } from 'mobx-react'
 
-
 @observer
 class App extends Component {
 
